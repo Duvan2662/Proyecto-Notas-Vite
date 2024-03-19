7ce70cf577d9d981c2305dc23575db5d17ba108f
Duvan <duvanriano2626@gmail.com>
2024-03-19 09:13:08 -0500

    Desarrollo de la funcionalidad de cada funcion

--- src/store/nota.store.js
+++ src/store/nota.store.js
@@ -2,7 +2,7 @@ import { Nota } from "../notas/models/notas.models";
 
 //Estados de las notas 
 const Filtros = {
-    Todo       : 'Todo',
+    Todas       : 'Todas',
     Completado : 'Completado',
     Pendiente  : 'Pendiente'
 }
@@ -14,7 +14,7 @@ const contenedorNotas = {
         new Nota('Hacer almuerzo'),
         new Nota('Hacer comida'),
     ],
-    filtros : Filtros.Todo
+    filtros : Filtros.Todas
 }
 
 const iniciarNotas = () =>{
@@ -26,37 +26,64 @@ const cargarNotas = () => {
     throw new Error ('No implementado');
 }
 
+//Funcion que me ayuda a mostrar las notas segun sus filtros 
+const obtenerNotas = (filtro = Filtros.Todas) =>{
+    switch (filtro) {
+        case Filtros.Todas:
+            return  [...contenedorNotas.notas];//Muestra todas las notas
+        case Filtros.Completado:
+            return contenedorNotas.notas.filter(nota => nota.estado)//Devuelve las notas que el estado sea true
+        case Filtros.Pendiente:
+            return contenedorNotas.notas.filter(nota => !nota.estado)//Devuelve las notas que el estado sea false    
+        default:
+            throw new Error (`El filtro ${filtro} no esta disponible`);   
+    }
+}
+
+//Funcion para añadir notas
 const anadirNota = (descripccion) => {
-    throw new Error ('No implementado');
+    if(!descripccion){
+        throw new Error ('No implementado');
+    }
+    contenedorNotas.notas.push(new Nota(descripccion));
 }
-
-const modificarNota = (notaId) => {
-    throw new Error ('No implementado');
+//Funcion para modificar notas
+const modificarEstadoNota = (notaId) => {
+    contenedorNotas.notas = contenedorNotas.notas.map(nota =>{
+        if(nota.id === notaId){
+            nota.estado = !nota.estado //Cambia el estado si es true pasa a false, si es false pasa a true 
+        }
+        return nota;
+    })
 }
+//Funcion para eliminar una nota
 const eliminarNota = (notaId) => {
-    throw new Error ('No implementado');
+    contenedorNotas.notas = contenedorNotas.notas.filter(nota => nota.id !== notaId); //Devuelve el arreglo de la notas con id diferente al ingresado
 }
 
+//Funcion para borrar una nota
 const borrarCompletados = () =>{
-    throw new Error ('No implementado');
-
+    contenedorNotas.notas = contenedorNotas.notas.filter(nota => nota.estado); //Devuelve el arreglo de la notas con el estado No completado = false
 }
 
-const establecerFiltro = (nuevoFiltro = Filtros.Todo) =>{
-    throw new Error ('No implementado');
+//Funcion para establecer un nuevo filtro en el contenedor
+const establecerFiltro = (nuevoFiltro = Filtros.Todas) =>{
+    contenedorNotas.filtros = nuevoFiltro;
 }
 
+//Funcion que me devulve un arreglo con un determinado filtro 
 const notasFiltro = () => {
-    throw new Error ('No implementado');
-
+    return contenedorNotas.filtros
 }
+
 
 
 export default {
     iniciarNotas,
+    obtenerNotas,
     cargarNotas,
     anadirNota,
-    modificarNota,
+    modificarEstadoNota,
     eliminarNota,
     borrarCompletados,
     establecerFiltro,
